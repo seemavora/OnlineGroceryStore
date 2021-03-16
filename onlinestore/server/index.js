ee59b2a5e22efe51a655abff886c97e1001456cf
Seema Vora <seema.s.vora@gmail.com>
2021-03-15 17:14:36 -0700

    Fixes Error With Express

--- onlinestore/server/index.js
+++ onlinestore/server/index.js
@@ -4,8 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server Started on Port: ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+
 app.get("/test", (req, res) => {
 
-  res.set("It works");
+  res.send("hello");
 });
