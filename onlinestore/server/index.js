0f6187270d364f12810b07064c78f79a26cdda5f
Seema Vora <seema.s.vora@gmail.com>
2021-03-17 01:20:36 -0700

    Finishes Mongoose and MongoDB Setup (part 2 tutorial)

--- onlinestore/server/index.js
+++ onlinestore/server/index.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
+// sets up server
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 
-app.get("/test", (req, res) => {
+// use the script to check if your server has been setup
+// app.get("/test", (req, res) => {
 
-  res.send("hello");
+//   res.send("hello");
+// });
+
+
+//connect to mongoDB
+mongoose.connect(process.env.MDB_CONNECT,{
+  useNewUrlParser: true,
+  useUnifiedTopology:true,
+}, (err) => {
+  if(err) return console.error(err);
+  console.log("Connected to MongoDB");
 });
+
