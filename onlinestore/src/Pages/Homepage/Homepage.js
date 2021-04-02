1e9de2aeaf8a00ee951c9f634cfe3802de57e6e1
Arman Sadeghi <arman1998@gmail.com>
2021-04-02 11:38:05 -0700

    added material UI

--- onlinestore/src/Pages/Homepage/Homepage.js
+++ onlinestore/src/Pages/Homepage/Homepage.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
-import './Homepage.css';
+import React, { Component } from 'react';
+import { Box, Container, Grid } from '@material-ui/core';
 class Homepage extends Component {
   render() {
     return (
-        <div>
-         <h1>
-           Homepage
-         </h1>
-         {/* add code for Homepage design here */}
-        </div>
+      <Container fluid>
+        <Grid container>
+          <Grid item xs={12} md={6}>Hello world</Grid>
+          <Grid item xs={12} md={6}>Hello world</Grid>
+        </Grid>
+      </Container>
     );
   }
 }
- 
+
 export default Homepage;
