a68411e21ad2e2a59460898d0ed9b4330b920970
Arman Sadeghi <arman1998@gmail.com>
2021-04-02 11:38:05 -0700

    added material UI

--- onlinestore/src/Pages/Homepage/Homepage.js
+++ onlinestore/src/Pages/Homepage/Homepage.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from "react";
 import "./Homepage.css";
 
@@ -8,6 +9,19 @@ class Homepage extends Component {
         <h1>Homepage</h1>
         {/* add code for Homepage design here */}
       </div>
+=======
+import React, { Component } from 'react';
+import { Box, Container, Grid } from '@material-ui/core';
+class Homepage extends Component {
+  render() {
+    return (
+      <Container fluid>
+        <Grid container>
+          <Grid item xs={12} md={6}>Hello world</Grid>
+          <Grid item xs={12} md={6}>Hello world</Grid>
+        </Grid>
+      </Container>
+>>>>>>> added material UI
     );
   }
 }
