1c2531087b1c1a5d8e0adba20120b003108dccd3
Nate Vu <natevu123@gmail.com>
2021-04-02 15:31:08 -0700

    combining image folders and simple edits

--- onlinestore/src/Pages/Homepage/BottomSections.js
+++ onlinestore/src/Pages/Homepage/BottomSections.js
@@ -1,40 +1,53 @@
 import React, { Component } from 'react';
 import { Box, Grid, Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+
+
+
 
 export default class BottomSections extends Component {
+  
   render() {
     return (
+
       <Grid container>
+
         <Grid item xs={12}>
             <Typography align="center" variant="h4">
-              Bottom Section
+              Bottom Section 
             </Typography>
         </Grid>
+
         <Grid item xs={3}>
           <Box py={20} bgcolor="#ff000059">
             <Typography align="center" variant="h4">
-              lorem ipsum
+              Our Mission
             </Typography>
           </Box>
         </Grid>
+        
         <Grid item xs={3}>
           <Box py={20} bgcolor="#00ffe759">
             <Typography align="center" variant="h4">
-              lorem ipsum
+              Check our Catalog
             </Typography>
           </Box>
         </Grid>
+
         <Grid item xs={3}>
           <Box py={20} bgcolor="#0043ff59">
             <Typography align="center" variant="h4">
-              lorem ipsum
+              Meet Our Team
             </Typography>
           </Box>
         </Grid>
+
         <Grid item xs={3}>
           <Box py={20} bgcolor="#1cff0059">
             <Typography align="center" variant="h4">
-              lorem ipsum
+              <Button variant="outlined" color="primary">
+              Sign Up Now
+            </Button>
             </Typography>
           </Box>
         </Grid>
