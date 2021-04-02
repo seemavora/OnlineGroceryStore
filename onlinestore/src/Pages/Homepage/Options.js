9748a862024a37d3b769309146081c806cea786e
Arman Sadeghi <arman1998@gmail.com>
2021-04-02 14:31:16 -0700

    added image to "Vegan"

--- onlinestore/src/Pages/Homepage/Options.js
+++ onlinestore/src/Pages/Homepage/Options.js
@@ -5,31 +5,49 @@ export default class Options extends Component {
   render() {
     return (
       <Grid container>
-        <Grid item xs={3}>
-          <Box py={20} bgcolor="#ff000059">
+        <Grid
+          item
+          xs={3}
+          style={{
+            '&:hover': {
+              transform: 'scale(1.2)',
+            },
+            transform: 'scale(1)',
+          }}
+        >
+          <Box
+            py={20}
+            bgcolor="#ff000059"
+            style={{
+              backgroundImage:
+                'url(https://img.hellofresh.com/f_auto,fl_lossy,h_710,q_auto,w_710/hellofresh_website/us/cms/homepage/Recipe%20Slider/Veggie_720x720.jpg)',
+              backgroundPosition: 'center',
+            }}
+            color="#fff"
+          >
             <Typography align="center" variant="h4">
-              lorem ipsum
+              Vegan
             </Typography>
           </Box>
         </Grid>
         <Grid item xs={3}>
           <Box py={20} bgcolor="#00ffe759">
             <Typography align="center" variant="h4">
-              lorem ipsum
+              Vegitarian
             </Typography>
           </Box>
         </Grid>
         <Grid item xs={3}>
           <Box py={20} bgcolor="#0043ff59">
             <Typography align="center" variant="h4">
-              lorem ipsum
+              Fish & Poultry
             </Typography>
           </Box>
         </Grid>
         <Grid item xs={3}>
           <Box py={20} bgcolor="#1cff0059">
             <Typography align="center" variant="h4">
-              lorem ipsum
+              Meat
             </Typography>
           </Box>
         </Grid>
