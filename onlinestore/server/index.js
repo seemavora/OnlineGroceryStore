927b260cd52ae907fc12ae5e328b3f26fcd20756
Seema Vora <seema.s.vora@gmail.com>
2021-04-10 17:17:04 -0700

    Adds Index.js Issue

--- onlinestore/server/index.js
+++ onlinestore/server/index.js
@@ -8,10 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
-<<<<<<< HEAD
-=======
 app.use(express.json()); // express allows you to read from json, if path is not specified
->>>>>>> 3de2d127a7f8037c659d45b5b92ac249fe4826c2
 
 // use the script to check if your server has been setup
 // app.get("/test", (req, res) => {
@@ -29,18 +26,9 @@ mongoose.connect(process.env.MDB_CONNECT,{
   console.log("Connected to MongoDB");
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 3de2d127a7f8037c659d45b5b92ac249fe4826c2
 // set up routes
 
 app.use("/auth", require("./routers/userRouter"));
 app.use("/customer", require("./routers/customerRouter"));
 
 
-<<<<<<< HEAD
->>>>>>> Adds the Middleware
-=======
->>>>>>> 3de2d127a7f8037c659d45b5b92ac249fe4826c2
