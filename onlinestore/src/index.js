8a87d86386954b7c859b4e1a2de1e869f8fa8341
Seema Vora <seema.s.vora@gmail.com>
2021-04-11 18:41:34 -0700

    Fixes Cors Certification, Login Functionality Works (inBackend, Frontend Started) pt 10

--- onlinestore/src/index.js
+++ onlinestore/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routing from './Routes'
 import './index.css';
+import axios from "axios";
 
+axios.defaults.withCredentials = true;
 
 
 export default function App(props) {
