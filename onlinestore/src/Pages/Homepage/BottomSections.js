16937fc7e76079f4db8034d7c9e8ad2fcea374f5
Nate Vu <natevu123@gmail.com>
2021-04-13 22:04:50 -0700

    edited homepage

--- onlinestore/src/Pages/Homepage/BottomSections.js
+++ onlinestore/src/Pages/Homepage/BottomSections.js
@@ -6,7 +6,7 @@ import {styled} from '@material-ui/core/styles';
 import './Homepage.css'
  
 const StylishBox = styled(Box)({
-  background: 'linear-gradient(45deg, #2f503d 20%, #abb94f 90%)',
+  background: 'linear-gradient(45deg, #2f503d 10%,#2f503d 90%, #abb94f 95%)',
   border: 0,
   borderRadius: 50,
   boxShadow: '0 3px 5px 2px rgba(165, 188, 163, .3)',
@@ -18,7 +18,7 @@ const StylishBox = styled(Box)({
 });
 
 const StylishButton = styled(Button)({
-  background: 'linear-gradient(45deg, #abb94f 10%, #2f503d 50%)',
+  background: 'linear-gradient(45deg, #2f503d 50%, #2f503d 50%)',
   border: 0,
   borderRadius: 3,
   boxShadow: '0 3px 5px 2px rgba(165, 188, 163, .3)',
@@ -33,20 +33,19 @@ const StylishButton = styled(Button)({
 function AboutUs() {
   return (
   <StylishBox> 
-    <Box align="left" m="0 auto" width="600px">
+    <Box align="left" m="0 auto">
     <Divider/>
-    <h3 align="center"></h3>
-    <h3 align="center">Here at OFS we value 3 things above all else</h3>
+    <h3 className="title" align="center">Here at OFS we value 3 things above all else</h3>
     
       <ul>
         <li>Fresh, Quality Foods</li>
         <li>Fast Shipping</li>
         <li>Friendly Customer Service</li>
       </ul>
-    <p align="left">We work our hardest to make sure that all our food is GMO free and comes from sustainable sources so that you can be confident that you're getting only the best ingredients.</p>
+    <p> We work our hardest to make sure that all our food is GMO free and comes from sustainable sources so that you can be confident that you're getting only the best ingredients.</p>
 
-    <h3 align="left">Our Story</h3>
-    <p align="left">Starting as a small, local grocery store in San Jose we realized that our customers didn't have a lot of time to spend
+    <h3 className="title">Our Story</h3>
+    <p>Starting as a small, local grocery store in San Jose we realized that our customers didn't have a lot of time to spend
     the aisles of a grocery store, especially in the high speed world of the Silicon Valley. </p>
     <p>Our solution was an online storefront so our customers can browse our selection from the comfort of home and have their food delievered right to their doorsteps.</p>
     <Divider/>
