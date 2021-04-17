566b0c3e9acacfa3abc7065317995ec424145d31
Andy Ngo <Andyngo28@gmail.com>
2021-04-16 17:47:25 -0700

    Pushing recent table changes

--- onlinestore/src/Pages/Inventory/Inventory.js
+++ onlinestore/src/Pages/Inventory/Inventory.js
@@ -1,18 +1,98 @@
-import React, { Component } from "react";
+import React, { useState }  from "react";
+import ReactDOM from 'react-dom'
 import './Inventory.css';
-import LogOutBtn from "../../Components/Buttons/LogOutBtn";
-class Inventory extends Component {
-  render() {
-    return (
-        <div>
-         <h1>
-           Inventory
-         <LogOutBtn/>
-         </h1>
-         {/* add code for Inventory design here */}
-        </div>
-    );
-  }
-}
+import MaterialTable from 'material-table'
+
+const dataList = [
+   { iD: 1, product: 'Strawberries', price: '$3.00', quantity: 30},
+   { iD: 2, product: 'Chicken Nuggets', price: '$10.00', quantity: 40 },
+]
+
+function Inventory(props) {
+
+   const [data, setData] = useState(dataList);
+
+   const columns = [       //[columns, setColumns] = useState
+     {
+       title: 'ID', field: 'id', type: 'numeric',
+       title: 'Product', field: 'product',
+       editComponent: props => (
+         <input
+           type="text"
+           value={props.value}
+           onChange={e => props.onChange(e.target.value)}
+         />
+       )
+     },
+     { title: 'Price', field: 'price' },
+     { title: 'Quantity', field: 'quantity', type: 'numeric' },
+   ];
+
+   return (
+     <MaterialTable
+       title="Admin Inventory Table"
+       columns={columns}
+       data={data}
+       options = {{
+         search: false,
+         paging: false,
+         filtering: true,
+         sorting: true
+       }}
+       editable={{
+         onRowAdd: newData =>
+           new Promise((resolve, reject) => {
+             setTimeout(() => {
+               setData([...data, newData]);
+               
+               resolve();
+             }, 1000)
+           }),
+
+         onRowUpdate: (newData, oldData) =>
+           new Promise((resolve, reject) => {
+             setTimeout(() => {
+               const dataUpdate = [...data];
+               const index = oldData.tableData.id;
+               dataUpdate[index] = newData;
+               setData([...dataUpdate]);
  
-export default Inventory;
+               resolve();
+             }, 1000)
+           }),
+
+         onRowDelete: oldData =>
+           new Promise((resolve, reject) => {
+             setTimeout(() => {
+               const dataDelete = [...data];
+               const index = oldData.tableData.id;
+               dataDelete.splice(index, 1);
+               setData([...dataDelete]);
+ 
+               resolve();
+             }, 1000)
+           }),
+       }}
+     />
+   )
+ }
+
+ export default Inventory;
+
+import React, { Component } from 'react';
+import MaterialTable from 'material-table'
+import {Table} from './Table';
+
+
+
+// function Inventory(){
+//    return (
+//       <div className = "Inventory">
+//          <h2> React-App</h2>
+//          <Table/>
+//       </div>
+//    )
+// }
+ 
+// export default Inventory;
+
