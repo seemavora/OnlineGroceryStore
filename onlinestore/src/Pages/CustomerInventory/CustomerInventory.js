9a2ad959a7ffc6158466cda02b5fee6feaef2826
Latrida <Latrida@Latridas-MacBook-Air.local>
2021-04-22 20:47:22 -0700

    Added CustomerInventory

---
+++ onlinestore/src/Pages/CustomerInventory/CustomerInventory.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./CustomerInventory.css";
+import MainContent from "../CustomerInventory/mainContent";
+
+function CustomerInventory() {
+  return (
+    <div className="container">
+      <MainContent />
+    </div>
+  );
+}
+
+export default CustomerInventory;
