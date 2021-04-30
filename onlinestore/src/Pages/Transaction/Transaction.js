a6245510ebdbeeb6e925d41fbf048945a2430133
Arman Sadeghi <arman1998@gmail.com>
2021-04-29 19:08:24 -0700

    added style (not working smh)

--- onlinestore/src/Pages/Transaction/Transaction.js
+++ onlinestore/src/Pages/Transaction/Transaction.js
@@ -1,65 +1,87 @@
 import React, { Component } from 'react';
 import Checkout from './Checkout';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 
+const useStyles = makeStyles((theme) => ({
+  listItem: {
+    padding: theme.spacing(1, 0),
+  },
+  total: {
+    fontWeight: 700,
+  },
+  title: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+function cardInfo() {
+  //const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Payment Method
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="cardName"
+            label="Name on card"
+            fullWidth
+            autoComplete="cc-name"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="cardName"
+            label="Card Number"
+            fullWidth
+            autoComplete="cc-number"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="cvv"
+            label="CVV"
+            helperText="Last three digits on the right side of the signature strip"
+            fullWidth
+            autoComplete="cc-csc"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="expDate"
+            label="Expiry Date"
+            fullWidth
+            autoComplete="cc-exp"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="cupon"
+            label="Cupon Code"
+            fullWidth
+            autoComplete="cc-cupon"
+          />
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+}
+
 export default class Transaction extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Typography variant="h6" gutterBottom>
-          Payment Method
-        </Typography>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6}>
-            <TextField
-              required
-              id="cardName"
-              label="Name on card"
-              fullWidth
-              autoComplete="cc-name"
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              required
-              id="cardName"
-              label="Card Number"
-              fullWidth
-              autoComplete="cc-number"
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              required
-              id="cvv"
-              label="CVV"
-              helperText="Last three digits on the right side of the signature strip"
-              fullWidth
-              autoComplete="cc-csc"
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              required
-              id="expDate"
-              label="Expiry Date"
-              fullWidth
-              autoComplete="cc-exp"
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              required
-              id="cupon"
-              label="Cupon Code"
-              fullWidth
-              autoComplete="cc-cupon"
-            />
-          </Grid>
-        </Grid>
-      </React.Fragment>
+      <Grid container>
+        <cardInfo />
+      </Grid>
     );
   }
 }
