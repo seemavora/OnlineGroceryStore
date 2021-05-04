657038d23cd30bca1c855a64f68f499ee6993102
Latrida <Latrida@Latridas-MacBook-Air.local>
2021-05-03 22:00:02 -0700

    Shopping Cart Screen

--- onlinestore/src/Routes.js
+++ onlinestore/src/Routes.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Homepage from './Pages/Homepage/Homepage'
-import Inventory from './Pages/Inventory/Inventory';
-import Login from './Pages/Login/Login';
-import SignUp from './Pages/SignUp/SignUp';
-import Navbar from './Components/Navbar/Navbar';
-import Contact from './Pages/Contact/Contact'; 
-import TransactionHistory from './Pages/TransactionHistory/TransactionHistory';
-export default function Routes(props){
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Homepage from "./Pages/Homepage/Homepage";
+import Inventory from "./Pages/Inventory/Inventory";
+import CustomerInventory from "./Pages/CustomerInventory/CustomerInventory";
+import Login from "./Pages/Login/Login";
+import SignUp from "./Pages/SignUp/SignUp";
+import Navbar from "./Components/Navbar/Navbar";
+import Contact from "./Pages/Contact/Contact";
+import CartScreen from "./Pages/Cart/CartScreen";
+export default function Routes(props) {
   const routes = [
     {
       path: "/",
@@ -30,10 +31,14 @@ export default function Routes(props){
       component: TransactionHistory
     },
     {
-      path:'/Contact',
-      component: Contact
-    }
-  ]
+      path: "/Contact",
+      component: Contact,
+    },
+    {
+      path: "/Cart",
+      component: CartScreen,
+    },
+  ];
 
   return (
     <Router>
