7eef1b663bad20161986e9d2a4fec6a72919d63e
Seema Vora <seema.s.vora@gmail.com>
2021-05-06 15:07:23 -0700

    Saves Admin in DB

--- server/routers/adminRouter.js
+++ server/routers/adminRouter.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
 const Admin = require("../models/adminModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 router.post("/",async (req, res) =>{
 
   try{
-    const {email, password,passwordVerify, accessCode } = req.body;
+    const {email, password, passwordVerify, accessCode } = req.body;
 
     //validation
-    if (!email || !password || !passwordVerify) {
+    if (!email || !password || !passwordVerify || !accessCode) {
       return res.status(400).json({ errorMessage: "Please enter all required fields." }); //400 = bad request
     }
     if (password.length < 6) {
@@ -16,14 +19,23 @@ router.post("/",async (req, res) =>{
       return res.status(400).json({ errorMessage: "Please enter same password twice." }); //400 = bad request
     }
     if (accessCode != "frankbutt") {
-      return res.status(400).json({ errorMessage: "Incorrect Access Code" }); //400 = bad request
+      return res.status(400).json({ errorMessage: "Please enter same password twice." }); //400 = bad request
     }
-    const existingAdmin = await Admin.findOne({ email });// checking if that user already exists
+    const existingAdmin = await Admin.findOne({ email });// checking if that admin already exists
     if (existingAdmin) {
       return res.status(400).json({ errorMessage: "Account with this email exists." }); //400 = bad request   
     }
 
-    console.log(existingAdmin);
+      // hashing the password
+      const salt = await bcrypt.genSalt(); //creates random string of numbers
+      const passwordHash = await bcrypt.hash(password,salt);
+    console.log(passwordHash)
+      //save a new admin account to database
+      const newAdmin = new Admin({email, passwordHash, accessCode});
+      const savedAdmin = await newAdmin.save();
+  
+      //log-in the admin
+      const token = jwt.sign({admin: savedAdmin._id,}, process.env.JWT_SECRET);
 
   }catch(err){
     console.error(err);
