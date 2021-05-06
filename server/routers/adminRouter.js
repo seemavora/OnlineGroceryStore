c83579775c8bd22ec4d2acb54ab387d4f1d873bd
Seema Vora <seema.s.vora@gmail.com>
2021-05-06 15:23:44 -0700

    Admin Router

--- server/routers/adminRouter.js
+++ server/routers/adminRouter.js
@@ -29,7 +29,7 @@ router.post("/",async (req, res) =>{
       // hashing the password
       const salt = await bcrypt.genSalt(); //creates random string of numbers
       const passwordHash = await bcrypt.hash(password,salt);
-    console.log(passwordHash)
+
       //save a new admin account to database
       const newAdmin = new Admin({email, passwordHash, accessCode});
       const savedAdmin = await newAdmin.save();
@@ -37,9 +37,69 @@ router.post("/",async (req, res) =>{
       //log-in the admin
       const token = jwt.sign({admin: savedAdmin._id,}, process.env.JWT_SECRET);
 
+     //sign cookie to send as a HTTP-only cookie
+     res.cookie("token", token,{
+      httpOnly:true,
+    }).send();
   }catch(err){
     console.error(err);
     res.status(500).send();
+  }
+});
+
+//login
+router.post("/adminlogin", async(req, res) =>{
+  try{
+    const {email, password} = req.body;
+
+    // validate
+    if(!email || !password){
+      return res.status(400).json({errorMessage: "Please enter all required fields."});
+    }
+    const existingAdmin = await Admin.findOne({email});
+    if(!existingAdmin){
+      return res.status(401).json({errorMessage: "Wrong email or password."}); // 401 = unauthorized
+    }
+    const passwordCorrect = await bcrypt.compare(password, existingAdmin.passwordHash);
+    if(!passwordCorrect){
+      return res.status(401).json({errorMessage: "Wrong email or password."});
+    }
+
+ //log-in the user
+ const token = jwt.sign({user: existingAdmin._id,}, process.env.JWT_SECRET);
+    
+ //sign cookie to send as a HTTP-only cookie
+ res.cookie("token", token,{
+   httpOnly:true,
+ }).send();
+  }catch(err){
+    console.error(err);
+    res.status(500).send(); // 500 = internal server error
+  }
+});
+
+//logout user
+router.get("/adminlogout", (req, res) =>{
+  res.cookie("token","",{
+    httpOnly : true,
+    expires: new Date(0)
+  }).send();
+});
+
+//checks if user is loggedin
+router.get("/adminloggedIn", (req, res) =>{
+  try{
+    //trying to read cookie from request (See if authorized)
+    const token = req.cookies.token; //object to store cookies
+    if(!token){
+      return res.status(200).json(false); //checks if user is logged in
+    }
+    jwt.verify(token, process.env.JWT_SECRET);//returns string/obj compares token to password, if token hasnt been created throw an error
+    res.send(true);
+
+  }catch(err){
+    console.error(err);
+    res.status(200).json(false);
   }
 });
 
