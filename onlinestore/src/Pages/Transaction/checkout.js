fe15aea25e286dfd4f3adcf664299f2046ce94f0
Nate Vu <natevu123@gmail.com>
2021-05-06 16:19:25 -0700

    address field completed (still want to figure out how to add a drop down menu but I cant quite figure it out)

--- onlinestore/src/Pages/Transaction/checkout.js
+++ onlinestore/src/Pages/Transaction/checkout.js
@@ -1,13 +1,125 @@
 import React, { Component } from 'react';
-import Typography from '@material-ui/core/Typography';
+import { Box, Grid, Typography, Divider} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import {styled, makeStyles} from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
+
+
+
+const AddressContainer = styled(Box)({
+    border: 0,
+    borderRadius: 5,
+    boxShadow: '0 3px 5px 2px rgba(165, 188, 163, .3)',
+    color: 'Black',
+    width: '50%',
+    height: 'auto',
+    padding: '0 30px',
+    marginTop: '50px',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+    
+});
+
+
+
+function AddressField(){
+  
+
+    return(
+    <AddressContainer>
+        <Typography variant="h6">Shipping Address</Typography>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <TextField
+                required
+                id="name"
+                name="name"
+                label="Full Name (First and Last name)"
+                fullWidth
+               
+                />
+            </Grid>
+        <Grid item xs={12}>
+            <TextField
+            id="phoneNumber"
+            name="phoneNumber"
+            label="Phone number"
+            fullWidth
+            />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="address1"
+            name="address1"
+            label="Address line 1"
+            helperText="Street address or P.O. Box"
+            fullWidth
+            
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="address2"
+            name="address2"
+            label="Address line 2"
+            helperText="Apt, suite, unit, building, floor, etc."
+            fullWidth
+            
+          />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <TextField
+            required
+            id="city"
+            name="city"
+            label="City"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <TextField id="state" name="state" label="State/Province/Region" fullWidth />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <TextField
+            required
+            id="zip"
+            name="zip"
+            label="Zip / Postal code"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} >
+          <TextField
+            type="readOnly"
+            id="country"
+            name="country"
+            label="Country"
+            defaultValue="United States"
+            InputProps={{
+                readOnly: true,
+            }}
+
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
+            label="Use this address for payment details"
+          />
+        </Grid>
+        </Grid>
+    </AddressContainer>
+    );
+}
 export default class Checkout extends Component {
+   
     render(){
         return (
-            <body>
-                hello
-            </body>
-              
+            <AddressField></AddressField>
           );
     }
 }
