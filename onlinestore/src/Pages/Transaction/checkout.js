2efcf09934232655bb0cb6d01ce56e78b55999d5
Nate Vu <natevu123@gmail.com>
2021-05-06 19:09:55 -0700

    drop down menu finally works!!!!!!!

--- onlinestore/src/Pages/Transaction/checkout.js
+++ onlinestore/src/Pages/Transaction/checkout.js
@@ -5,6 +5,237 @@ import { styled, makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import MenuItem from '@material-ui/core/MenuItem';
+
+
+const usStates = [
+  {
+    value: 'AL',
+    label: 'Alabama',
+  },
+  {
+    value: 'AK',
+    label: 'Alaska',
+  },
+  {
+    value: 'AS',
+    label: 'American Samoa',
+  },
+  {
+    value: 'AR',
+    label: 'Arizona',
+  },
+  {
+    value: 'CA',
+    label: 'California',
+  },
+  {
+    value: 'CO',
+    label: 'Colorado',
+  },
+  {
+    value: 'CT',
+    label: 'Connecticut',
+  },
+  {
+    value: 'DE',
+    label: 'Delaware',
+  },
+  {
+    value: 'DC',
+    label: 'District of Columbia',
+  },
+  {
+    value: 'FL',
+    label: 'Florida',
+  },
+  {
+    value: 'GA',
+    label: 'Georgia',
+  },
+  {
+    value: 'GU',
+    label: 'Guam',
+  },
+  {
+    value: 'HI',
+    label: 'Hawaii',
+  },
+  {
+    value: 'ID',
+    label: 'Idaho',
+  },
+  {
+    value: 'IL',
+    label: 'Illinois',
+  },
+  {
+    value: 'IN',
+    label: 'Indiana',
+  },
+  {
+    value: 'IA',
+    label: 'Iowa',
+  },
+  {
+    value: 'KS',
+    label: 'Kansas',
+  },
+  {
+    value: 'KY',
+    label: 'Kentucky',
+  },
+  {
+    value: 'LA',
+    label: 'Louisiana',
+  },
+  {
+    value: 'ME',
+    label: 'Maine',
+  },
+  {
+    value: 'MD',
+    label: 'Maryland',
+  },
+  {
+    value: 'MA',
+    label: 'Massachusetts',
+  },
+  {
+    value: 'MI',
+    label: 'Michigan',
+  },
+  {
+    value: 'MN',
+    label: 'Minnesota',
+  },
+  {
+    value: 'MS',
+    label: 'Mississippi',
+  },
+  {
+    value: 'MO',
+    label: 'Missouri',
+  },
+  {
+    value: 'MT',
+    label: 'Montana',
+  },
+  {
+    value: 'NE',
+    label: 'Nebraska',
+  },
+  {
+    value: 'NV',
+    label: 'Nevada',
+  },
+  {
+    value: 'NH',
+    label: 'New Hampshire',
+  },
+  {
+    value: 'NJ',
+    label: 'New Jersey',
+  },
+  {
+    value: 'NM',
+    label: 'New Mexico',
+  },
+  {
+    value: 'NY',
+    label: 'New York',
+  },
+  {
+    value: 'NC',
+    label: 'North Carolina',
+  },
+  {
+    value: 'ND',
+    label: 'North Dakota',
+  },
+  {
+    value: 'OH',
+    label: 'Ohio',
+  },
+  {
+    value: 'OK',
+    label: 'Oklahoma',
+  },
+  {
+    value: 'OR',
+    label: 'Oregon',
+  },
+  {
+    value: 'PA',
+    label: 'Pennsylvania',
+  },
+  {
+    value: 'PR',
+    label: 'Puerto Rico',
+  },
+  {
+    value: 'RI',
+    label: 'Rhode Island',
+  },
+  {
+    value: 'SC',
+    label: 'South Carolina',
+  },
+  {
+    value: 'SD',
+    label: 'South Dakota',
+  },
+  {
+    value: 'TN',
+    label: 'Tennessee',
+  },
+  {
+    value: 'TX',
+    label: 'Texas',
+  },
+  {
+    value: 'UT',
+    label: 'Utah',
+  },
+  {
+    value: 'VT',
+    label: 'Vermont',
+  },
+  {
+    value: 'VI',
+    label: 'Virgin Islands',
+  },
+  {
+    value: 'VA',
+    label: 'Virginia',
+  },
+  {
+    value: 'WA',
+    label: 'Washington',
+  },
+  {
+    value: 'WV',
+    label: 'West Virginia',
+  },
+  {
+    value: 'WI',
+    label: 'Wisconsin',
+  },
+  {
+    value: 'WY',
+    label: 'Wyoming',
+  },
+];
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 const AddressContainer = styled(Box)({
   border: 0,
@@ -19,7 +250,14 @@ const AddressContainer = styled(Box)({
   marginRight: 'auto',
 });
 
+
 function AddressField() {
+  const classes = useStyles();
+  const [usState, setState] = React.useState('');
+
+  const handleChange = (event) => {
+    setState(event.target.value);
+  };
   return (
     <AddressContainer>
       <Typography variant="h6">Shipping Address</Typography>
@@ -61,15 +299,32 @@ function AddressField() {
           />
         </Grid>
         <Grid item xs={12} sm={4}>
-          <TextField required id="city" name="city" label="City" fullWidth />
+          <TextField 
+          required 
+          id="city"
+          name="city"
+          label="City"
+          fullWidth />
         </Grid>
         <Grid item xs={12} sm={4}>
-          <TextField
-            id="state"
-            name="state"
-            label="State/Province/Region"
-            fullWidth
-          />
+        <form className={classes.root} noValidate autoComplete="off">
+      
+        <TextField
+          id="standard-select-currency"
+          select
+          required
+          label="Select"
+          value={usState}
+          onChange={handleChange}
+          helperText="Please Select your State"
+        >
+          {usStates.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        </form>
         </Grid>
         <Grid item xs={12} sm={4}>
           <TextField
@@ -104,6 +359,7 @@ function AddressField() {
     </AddressContainer>
   );
 }
+
 export default class Checkout extends Component {
   render() {
     return <AddressField></AddressField>;
