07ff4aa1a30f10d4a821ec0add843cfebe541544
Andy Ngo <Andyngo28@gmail.com>
2021-05-06 21:21:21 -0700

    New branch to fix cart and inventory

--- onlinestore/src/Pages/Inventory/Inventory.js
+++ onlinestore/src/Pages/Inventory/Inventory.js
@@ -5,7 +5,7 @@ import MaterialTable from "material-table";
 
 const dataList = [
    { id: 1, product: 'Chocolate', weight: '20 oz', price: '$3.00', quantity: 30},
-   { id: 2, product: 'Chicken Nuggets', weight: '3 lbs', price: '$15.00', quantity: 40 },
+   { id: 2, product: 'Chicken Nuggets', weight: '4 lbs', price: '$5.00', quantity: 40 },
 ]
 
 function Inventory(props) {
@@ -29,6 +29,7 @@ function Inventory(props) {
         />
       ),
     },
+    { title: "Weight", field: "weight"},
     { title: "Price", field: "price" },
     { title: "Quantity", field: "quantity", type: "numeric" },
   ];
@@ -82,17 +83,4 @@ function Inventory(props) {
 
 export default Inventory;
 
-// import React, { Component } from 'react';
-// import MaterialTable from 'material-table'
-// import {Table} from './Table';
 
-// function Inventory(){
-//    return (
-//       <div className = "Inventory">
-//          <h2> React-Inventory</h2>
-//          <Table/>
-//       </div>
-//    )
-// }
-
-// export default Inventory;
