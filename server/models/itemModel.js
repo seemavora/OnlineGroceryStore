0f07f66b6c3a2d6aecec583ce5ce7078147d5cbb
Seema Vora <seema.s.vora@gmail.com>
2021-05-09 10:35:28 -0700

    Adds Items to inventory

--- server/models/itemModel.js
+++ server/models/itemModel.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const itemSchema = new mongoose.Schema({
   name:{type: String, required: true},
+  weight:{type: String, required:true},
+  price:{ type:String, require:true},
+  quantity:{type:String, require:true},
+  description:{type:String, require:true}
 });
 
 const Item = mongoose.model("item", itemSchema);
--- server/routers/itemRouter.js
+++ server/routers/itemRouter.js
@@ -4,10 +4,13 @@ const auth = require("../middleware/auth");
 
 router.post("/",auth, async(req, res) => { //auth function addes middleware, first run request to auth
   try{
-    const {name} = req.body;
+    const {name, weight, price, quantity, description} = req.body;
     const newItem = new Item({
-      name,
+      name,weight,price, quantity, description
     });
+    if (!name || !weight || !price || !quantity || !description) {
+      return res.status(400).json({ errorMessage: "Please enter all required fields." }); //400 = bad request
+    }
     const savedItem =  await newItem.save();
 
     res.json(savedItem);
